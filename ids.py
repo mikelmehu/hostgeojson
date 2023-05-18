415834118bed36939ded2758ebe010b3478bc744
root <root@vps.server.local>
2023-05-19 00:52:14 +0200

    Update from ssh

--- ids.py
+++ ids.py
@@ -1,6 +1,6 @@
 import json
 
-file = open("clientes.json", "r")
+file = open("clientes_nuevo.json", "r")
 data = json.loads(file.read())
 file.close()
 id = 1
@@ -10,6 +10,6 @@ for item in data["clientes"]:
     datos.append(item)
     id = id + 1
 
-f = open("clientes_id.json", "a")
+f = open("clientes_id_nuevo.json", "a")
 json.dump(datos, f)
 f.close()
